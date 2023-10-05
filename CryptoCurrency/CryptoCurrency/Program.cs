d53d90536902bdc21ddb293422df71d10344902e
alannyled <84908245+alannyled@users.noreply.github.com>
2023-10-05 11:19:45 +0200

    sådan

--- CryptoCurrency/CryptoCurrency/Program.cs
+++ CryptoCurrency/CryptoCurrency/Program.cs
@@ -1,6 +1,36 @@
 ﻿public class Converter {
-    //Main method
-    public static void Main(string[] args) {}
+
+    private Dictionary<string, double> _cryptoPrices = new Dictionary<string, double>(StringComparer.OrdinalIgnoreCase);
+    
+        public static void Main(string[] args) {
+        // Bare et eksempel til konsollen
+        Converter converter = new();
+        // Indtast navn og pris for første valuta
+        Console.WriteLine("Indtast navn på fra kryptovaluta:");
+        string? firstCurrencyName = Console.ReadLine();
+
+        Console.WriteLine($"Indtast prisen på en enhed af {firstCurrencyName} i USD:");
+        double firstCurrencyPrice;
+        while (!double.TryParse(Console.ReadLine(), out firstCurrencyPrice) || firstCurrencyPrice <= 0) {
+            Console.WriteLine("Indtast en gyldig pris (større end 0):");
+        }
+        converter.SetPricePerUnit(firstCurrencyName, firstCurrencyPrice);
+
+        // Indtast navn og pris for anden valuta
+        Console.WriteLine("\nIndtast navn på til kryptovaluta:");
+        string? secondCurrencyName = Console.ReadLine();
+
+        Console.WriteLine($"Indtast prisen på en enhed af {secondCurrencyName} i USD:");
+        double secondCurrencyPrice;
+        while (!double.TryParse(Console.ReadLine(), out secondCurrencyPrice) || secondCurrencyPrice <= 0) {
+            Console.WriteLine("Indtast en gyldig pris (større end 0):");
+        }
+        converter.SetPricePerUnit(secondCurrencyName, secondCurrencyPrice);
+
+        // Udfør konvertering
+        double result = converter.Convert(firstCurrencyName, secondCurrencyName, 1);
+        Console.WriteLine($"\n1 {firstCurrencyName} er lig med {result} {secondCurrencyName}.");
+    }
 
     /// <summary>
     /// Angiver prisen for en enhed af en kryptovaluta. Prisen angives i dollars.
@@ -10,7 +40,10 @@
     /// <param name="currencyName">Navnet på den kryptovaluta der angives</param>
     /// <param name="price">Prisen på en enhed af valutaen målt i dollars. Prisen kan ikke være negativ</param>
     public void SetPricePerUnit(String currencyName, double price) {
-
+        if (price <= 0) {
+                    throw new ArgumentException("Prisen kan ikke være 0 eller negativ.");
+                }
+                _cryptoPrices[currencyName] = price;
     }
 
     /// <summary>
@@ -23,6 +56,14 @@
     /// <param name="amount">Beløbet angivet i valutaen angivet i fromCurrencyName</param>
     /// <returns>Værdien af beløbet i toCurrencyName</returns>
     public double Convert(String fromCurrencyName, String toCurrencyName, double amount) {
-        return 0;
+        if (!_cryptoPrices.ContainsKey(fromCurrencyName)) {
+            throw new ArgumentException($"Kryptovaluta {fromCurrencyName} eksisterer ikke.");
+        }
+        if (!_cryptoPrices.ContainsKey(toCurrencyName)) {
+            throw new ArgumentException($"Kryptovaluta {toCurrencyName} eksisterer ikke.");
+        }
+
+        double usdValue = amount * _cryptoPrices[fromCurrencyName];
+        return usdValue / _cryptoPrices[toCurrencyName];
     }
 }
--- CryptoCurrency/CryptoCurrency/ProgramTest.cs
+++ CryptoCurrency/CryptoCurrency/ProgramTest.cs
@@ -1,6 +1,64 @@
 using Xunit;
 
 public class ProgramTest {
-
     
+    // Ækvivalensklasse: Gyldige værdier (SetPricePerUnit metoden)
+    [Fact]
+    public void Test_SetPricePerUnit_ValidPrice()
+    {
+        var converter = new Converter();
+        converter.SetPricePerUnit("litecoin", 100);
+        converter.SetPricePerUnit("bitcoin", 10000);
+        Assert.True(converter.Convert("litecoin", "bitcoin", 1) > 0);
+    }
+
+     // Ækvivalensklasse: Ugyldige værdier (Negativ pris og 0 i SetPricePerUnit)
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(0)]
+    public void Test_SetPricePerUnit_InvalidPrice_ThrowsException(double price)
+    {
+        var converter = new Converter();
+        Assert.Throws<ArgumentException>(() => converter.SetPricePerUnit("litecoin", price));
+    }
+
+    // Ækvivalensklasse: Gyldige værdier (Convert metoden)
+    [Fact]
+    public void Test_Convert_ValidCurrencies()
+    {
+        var converter = new Converter();
+        converter.SetPricePerUnit("litecoin", 100);
+        converter.SetPricePerUnit("bitcoin", 10000);
+        var result = converter.Convert("litecoin", "bitcoin", 1);
+        Assert.Equal(0.01, result, 5);
+    }
+
+    // Ækvivalensklasse: Ugyldige værdier (Ugyldigt 'fromCurrencyName' i Convert)
+    [Fact]
+    public void Test_Convert_InvalidFromCurrency_ThrowsException()
+    {
+        var converter = new Converter();
+        converter.SetPricePerUnit("bitcoin", 10000);
+        Assert.Throws<ArgumentException>(() => converter.Convert("invalidCurrency", "bitcoin", 10));
+    }
+
+    // Ækvivalensklasse: Ugyldige værdier (Ugyldigt 'toCurrencyName' i Convert)
+    [Fact]
+    public void Test_Convert_InvalidToCurrency_ThrowsException()
+    {
+        var converter = new Converter();
+        converter.SetPricePerUnit("bitcoin", 10000);
+        Assert.Throws<ArgumentException>(() => converter.Convert("bitcoin", "invalidCurrency", 10));
+    }
+
+    // Ækvivalensklasse: Ugyldige værdier (0 antal i Convert)
+    [Fact]
+    public void Test_Convert_InvalidAmount_ThrowsException()
+    {
+        var converter = new Converter();
+        converter.SetPricePerUnit("bitcoin", 10000);
+        converter.SetPricePerUnit("ethereum", 500);
+        var result = converter.Convert("bitcoin", "ethereum", 0);
+        Assert.True(result == 0);
+    }
 }
